156867f7bdde0356fb03433987250b42dc5eb687
SemiDoge <taelforlife@gmail.com>
2022-09-22 20:04:52 -0400

    Add a class to handle texture loading

---
+++ src/textureManager.cpp
@@ -0,0 +1,24 @@
+#include "../inc/textureManager.h"
+
+SDL_Texture * TextureManager::LoadTexture(const char * strFileName, SDL_Renderer * sdlRenderer) {
+
+    SDL_Surface * tmpSurface = nullptr;
+    SDL_Texture * texture = nullptr;
+
+
+    if ((tmpSurface = IMG_Load(strFileName)) == NULL) {
+        log(fmt::format("Failed to create surface, reason: {}", IMG_GetError()), logSeverity::ERROR);
+        return NULL;
+    }
+
+    if((texture = SDL_CreateTextureFromSurface(sdlRenderer, tmpSurface)) == NULL) {
+        log(fmt::format("Failed to create texture from: {}", strFileName), logSeverity::ERROR);
+        SDL_FreeSurface(tmpSurface);
+
+        return NULL;
+    }
+
+    SDL_FreeSurface(tmpSurface);
+
+    return texture;
+}
