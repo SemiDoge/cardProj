01decc5e90e3b67e8a4d78777d4ace5f850a6242
SemiDoge <taelforlife@gmail.com>
2023-04-26 01:29:41 -0400

    Fix incorrect CARD_STACK_WIDTH
    Also removed unused debug code

--- inc/defines.hpp
+++ inc/defines.hpp
@@ -4,7 +4,7 @@
 #define GLOBAL_SCALE 15
 #define SRC_CARD_WIDTH 21
 #define SRC_CARD_HEIGHT 30
-#define SRC_CARD_STACK_WIDTH 32
+#define SRC_CARD_STACK_WIDTH 27
 #define Z_INDEX_MAX 255
 
 #endif
--- src/zsorting.cpp
+++ src/zsorting.cpp
@@ -1,13 +1,8 @@
 #include "../inc/zsorting.hpp"
 
 void zidxSort(std::vector<std::shared_ptr<Entity>>& entities) {
+    Logger::log(fmt::format("Sorting rendering precedence."), logSeverity::INFO);
     std::sort(entities.begin(), entities.end(), zComp);
-
-    int i = 0;
-    for(std::shared_ptr<Entity> e : entities) {
-        Logger::log(fmt::format("[{}]: Z:{} ({})", i, e->GetZIndex(), e->GetEntityLabel()), logSeverity::TRACE);
-        i++;
-    }
 }
 
 bool zComp(const std::shared_ptr<Entity> a, const std::shared_ptr<Entity> b) {
